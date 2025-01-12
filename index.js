720e5e92bbb1732f7ee62f7b4e5c0fe44ed061f1
Jonas Almeida <jalmeida@mathbiol.org>
2025-01-12 14:58:14 -0500

    defer

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log(`loaded index.js \n ${Date()}`)
+
+// tip options
+const pCs=[0,5,10,15,20,25]
+pCs.forEach(pc=>{
+    console.log(pc)
+    opt = document.createElement('option')
+    tip.appendChild(opt)
+    opt.textContent=`${pc} %`
+    opt.value = pc
+})
+tip.value=20
+
