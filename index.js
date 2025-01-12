e920441af3842f6ffefff6bf8f735572996de131
Jonas Almeida <jalmeida@mathbiol.org>
2025-01-12 18:46:43 -0500

    calculation

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ pCs.forEach(pc=>{
     opt.textContent=`${pc} %`
     opt.value = pc
 })
-tip.value=20
+tip.value=20 // default
 
+// provide nominal cost value
+function calculate(){
+    let extra = Math.round(parseFloat(bill.value)*tip.value/100)
+    txt.value = `-- integer tip --`
+    txt.value += `\n`
+    txt.value += `\nBill: $${bill.value}`
+    txt.value += `\nTip: $${extra}`
+    txt.value += `\n`
+    txt.value += `\nTotal: $${parseFloat(bill.value)+extra}`
+    return txt
+}
+
+// calculation listener
+tip.onchange=bill.onkeyup=calculate
