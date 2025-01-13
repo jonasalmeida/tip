6cd93fde2f70155e5a910a2d5e6f6ea10ab0adee
Jonas Almeida <jalmeida@mathbiol.org>
2025-01-13 09:26:47 -0500

    light gray

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ tip.value=20 // default
 // provide nominal cost value
 function calculate(){
     let extra = Math.round(parseFloat(bill.value)*tip.value/100)
-    txt.value = `-- integer tip --`
+    txt.value = `\n -- integer tip --`
     txt.value += `\n`
-    txt.value += `\nBill: $${bill.value}`
-    txt.value += `\nTip: $${extra}`
+    txt.value += `\n Bill: $${bill.value}`
+    txt.value += `\n Tip: $${extra}`
     txt.value += `\n`
-    txt.value += `\nTotal: $${parseFloat(bill.value)+extra}`
+    txt.value += `\n Total: $${parseFloat(bill.value)+extra}`
     return txt
 }
 
